d42bc085068648b0a8089d0d929416a72df2f167
Sebastian Rapetti <sebastian.rapetti@alice.it>
2015-08-22 19:00:26 +0200

    Directory name

---
+++ Temp/App/Views/_templates/footer.php
@@ -0,0 +1,14 @@
+<footer>
+  
+</footer>
+<?php
+foreach ($js as $file) {
+    ?>
+<script src="<?php echo $file;
+    ?>"></script>
+<?php
+
+}
+?>
+</body>
+</html>
---
+++ Temp/App/Views/_templates/header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $title; ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <script>
+            var url = "<?php echo URL; ?>";
+        </script>
+
+        <?php
+        foreach ($css as $file) {
+            ?>
+        <link href="<?php echo $file;
+            ?>" rel="stylesheet" />
+        <?php
+
+        }
+        ?>
+    </head>
+    <body>
+        <header>
+            
+            <a class="menu icon home" href="#"></a>
+            <a class="menu" href="<?php echo URL; ?>">home</a>
+            <?php if ($data->isLogged === false) {
+    ?>
+            <a class="menu" href="<?php echo URL;
+    ?>login">login</a>
+            <?php 
+}?>
+            
+            <?php if ($data->isLogged === true) {
+    ?>
+            <a class="menu" href="<?php echo URL;
+    ?>logout">logout</a>
+            <?php 
+}?>
+            
+            <a class="menu" href="<?php echo URL; ?>protectedPage">protected page test</a>
+            <a class="menu" href="<?php echo URL; ?>user">users</a>
+            
+        </header>
+
+        <nav>
+            <div>
+
+            </div>
+            <div>
+                <ul>
+                    <li><h3>Welcome</h3></li>
+                    <li><a href="<?php echo URL; ?>">home</a></li>
+                    
+                    <?php if ($data->isLogged === false) {
+    ?>
+                    <li><a href="<?php echo URL;
+    ?>login">login</a></li>
+                    <?php 
+}?>
+                    
+                    <?php if ($data->isLogged === true) {
+    ?>
+                    <li><a href="<?php echo URL;
+    ?>login">logout</a></li>
+                    <?php 
+}?>
+                    
+                    <li><a href="<?php echo URL; ?>protectedPage">protected page test</a></li>
+                    <li><a href="<?php echo URL; ?>user">users</a></li>
+                </ul>
+               
+            </div>
+            <div>
+                <a class="closeNav" href="#">close</a>
+            </div>
+        </nav>
+
+
