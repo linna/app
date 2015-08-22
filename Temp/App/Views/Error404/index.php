a0ab0e45f78a758a118f7819aa39c6c08c4645ec
Sebastian Rapetti <sebastian.rapetti@alice.it>
2015-08-22 19:07:34 +0200

    Directory name

--- Temp/App/Views/Error404/index.php
+++
@@ -1,5 +0,0 @@
-<main>
-    <h1>Error 404</h1>
-    <p>This is the 404 Error-page. Will be shown when a page (controller or method) does not exist.</p>
-    <p><a href="<?php echo URL ?>">Return to Home</a> please.</p>
-</main>
