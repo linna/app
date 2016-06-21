03ac9f0f03199a5ba38fdec1f8f31a90c36a9a8f
Sebastian Rapetti <sebastian.rapetti@alice.it>
2016-06-21 21:49:58 +0200

    Html fix

--- App/Views/ProtectedPage/index.php
+++ App/Views/ProtectedPage/index.php
@@ -2,6 +2,6 @@
     <h1>You are inside a reserved area :)</h1>
     <p>In a real application this could be a login protected page.</p>
     
-    <p>Also this is a test for login system, you are logged like <b><?php echo $data->userName ?></b></p>
+    <p>Also this is a test for login system, you are logged as <b><?php echo $data->userName ?></b></p>
     <p>For disconnect:  <a href="<?php echo URL.'logout' ?>">Logout</a> </p>
 </main>
