99d51e712682e8fb02d345dd1c489cc9a6635053
Sebastian Rapetti <sebastian.rapetti@alice.it>
2016-09-15 21:36:25 +0200

    update config

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+include dirname(__DIR__).'/vendor/autoload.php';
+
+/*
+$loader = new \Linna\Autoloader();
+$loader->register();
+
+$loader->addNamespaces([
+    ['Linna', dirname(__DIR__) . '/src/Linna'],
+    ['Linna\Auth', dirname(__DIR__) . '/src/Linna/Auth'],
+    ['Linna\DI', dirname(__DIR__) . '/src/Linna/DI'],
+    ['Linna\Database', dirname(__DIR__) . '/src/Linna/Database'],
+    ['Linna\Http', dirname(__DIR__) . '/src/Linna/Http'],
+    ['Linna\Mvc', dirname(__DIR__) . '/src/Linna/Mvc'],
+    ['Linna\Session', dirname(__DIR__) . '/src/Linna/Session'],
+]);
+*/
